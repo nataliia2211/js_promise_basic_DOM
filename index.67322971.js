1e55f2c6d4b56d527370b50ef53d98f11aab8bff
Nataliia Fedorchuk <natali.fedorchuk2211@gmail.com>
2024-12-12 22:50:09 +0200

    make build

---
+++ index.67322971.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("logo"),t=new Promise(function(t){e.addEventListener("click",function(){t()})}),n=new Promise(function(e,t){setTimeout(function(){t(Error())},3e3)}),o=function(){var e=document.createElement("div");e.className="message",e.textContent="Promise was resolved!",document.body.appendChild(e)},c=function(){var e=document.createElement("div");e.className="message error-message",e.textContent="Promise was rejected!",document.body.appendChild(e)};t.then(o).catch(c),n.then(o).catch(c);
+//# sourceMappingURL=index.67322971.js.map
